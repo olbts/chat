a3b69bb013b7b1732d0a2818fab87abd28be872f
olbts <olaloum555@gmail.com>
2024-04-04 17:41:47 +0200

    mi chiamo chiesa

--- chat.js
+++ chat.js
@@ -5,21 +5,25 @@
     const buttonDeuxieme = document.querySelector("#buttonDeuxieme")
     const chat = document.querySelector("#chat")
     const date =  new Date()
-    //on crée un evenement quand on clique sur l element buttonPremier 
-    //ce que contient la fonction s'éxecute
    function time(){
      return date.getHours()  + ":" + date.getMinutes()  ;
-}
+    }
+    function isset(x){
+        return x.value != "";
+    }
    function premierePersonneFonction(){
-    currentTime = time();
-    console.log(currentTime);
-    chat.innerHTML =  chat.innerHTML + "<div class='red messageHolder'><span class='date'>"+currentTime+"</span>" + inputPremier.value + "</div>";
+       if(isset(inputPremier)){
+        currentTime = time();
+        chat.innerHTML =  chat.innerHTML + "<div class='red messageHolder'><span class='date'>"+currentTime+"</span>" + inputPremier.value + "</div>";
         inputPremier.value = "";
-   }
+    }
+}
     function deuxiemePersonneFonction (){
+        if(isset(inputDeuxieme)){
         currentTime = time();
         chat.innerHTML =  chat.innerHTML + "<div class='blue messageHolder'><span class='date'>"+currentTime+"</span>"+  inputDeuxieme.value + "</div>";
         inputDeuxieme.value = "";
     }
-    buttonPremier.addEventListener("click",premierePersonneFonction)
-    buttonDeuxieme.addEventListener("click",deuxiemePersonneFonction) 
+}
+buttonPremier.addEventListener("click",premierePersonneFonction)
+buttonDeuxieme.addEventListener("click",deuxiemePersonneFonction) 
